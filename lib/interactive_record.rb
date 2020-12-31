740360e0eaa591eb263f67a01d6aba4faa5e22b1
Sean Bigelow <sean.bigelow1@gmail.com>
2020-12-31 18:17:10 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -19,6 +19,13 @@ class InteractiveRecord
     column_names.compact
   end
 
+  def initialize(options={})
+  options.each do |property, value|
+    self.send("#{property}=", value)
+    end
+  end
+
+
   self.column_names.each do |col_name|
     attr_accessor col_name.to_sym
   end
